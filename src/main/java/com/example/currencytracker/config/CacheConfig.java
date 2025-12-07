975103e1e3783ecdf448c695baf38e1b6314e4d3
Linney34 <tarfedof@gmail.com>
2025-12-07 23:29:00 +0100

    Added CacheConfig and SwaggerConfig

---
+++ src/main/java/com/example/currencytracker/config/CacheConfig.java
@@ -0,0 +1,26 @@
+package com.example.currencytracker.config;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(1, TimeUnit.HOURS) // cache expiry
+                .maximumSize(1000);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("latestRate");
+        cacheManager.setCaffeine(caffeine);
+        return cacheManager;
+    }
+}
